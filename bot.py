87408b08fd8d0c9b7d7283a10f634d5967d4ee92
kaizenog69 <kaizenog69@gmail.com>
2026-03-25 00:42:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ import aiohttp
 # CONFIGURATION
 # ==========================================
 TELEGRAM_BOT_TOKEN = "8473908662:AAHTqwiz-hAmUKlNYgrrp2p0RrINU-sTpU8"
-WHATSAPP_API_URL = os.getenv("WHATSAPP_API_URL", "http://localhost:3001")
+WHATSAPP_API_URL = "WHATSAPP_API_URL=http://localhost:3000"
 
 # Owner ID - has full access
 OWNER_ID = 5926172220,5668200230
